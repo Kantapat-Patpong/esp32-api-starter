bfc5bc9594fbae4d8b4439534be0f0fcffe1deb7
Kantapat-Patpong <kpongpat2557@gmail.com>
2025-04-11 13:16:04 +0700

    config some main API

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// ใช้ชื่อให้ตรงกัน
 const sensorRoute = require('./routes/sensor');
 
-app.use(express.json());
+app.use(express.json()); // ต้องมาก่อน route ที่ใช้งาน body
+
+// ใช้ path /data กับ sensorRoute
 app.use('/data', sensorRoute);
 
+// หน้าหลัก
 app.get('/', (req, res) => {
   res.send('Welcome to ESP32 API Server 🌍');
 });
